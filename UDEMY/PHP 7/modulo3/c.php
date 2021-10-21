defe17d910858ea63bdd1e3f657e2840276798d8
Luciara-Abreu <luciara.abreu@gmail.com>
2021-10-20 21:23:43 -0300

    Atribuicoes

--- UDEMY/PHP 7/modulo3/c.php
+++ UDEMY/PHP 7/modulo3/c.php
@@ -5,8 +5,36 @@
 
 $title = 'Atribuições';
 echo 'Aqui atribuimos a variável $title o valor de => '.$title;
-echo '----------------------------------------';
+echo '<br> --------------------------------------------------------------- <br>';
 
+$numero = 10;
+echo '<br> Variável recebe Numero inteiro =' . $numero;
+$numero = $numero - 3;
+echo '<br> Variável recebe Numero -3 =' . $numero;
+$numero = $numero + 1.5;
+echo '<br> Variável recebe Numero + 1.5 =' . $numero;
+$numero --;
+echo '<br> Variável recebe Numero decremento =' . $numero;
+--$numero ;
+echo '<br> Variável recebe Numero decremento antes da Variável =' . $numero;
+$numero++;
+echo '<br> Variável recebe Numero incremento =' . $numero;
+++$numero;
+echo '<br> Variável recebe Numero incremento antes da variavel =' . $numero;
+$numero = 20;
+echo '<br> Variável recebe Numero Vinte =' . $numero;
+$numero -=5;
+echo '<br> Variável recebe decremento de 5 =' . $numero;
+$numero +=5;
+echo '<br> Variável recebe atribuição aditiva =' . $numero;
+$numero *=10;
+echo '<br> Variável recebe atribuição multiplicativa =' . $numero;
+$numero /=2;
+echo '<br> Variável recebe atribuição divisiva =' . $numero;
+$numero %=6;
+echo '<br> Variável recebe atibuição de modulo =' . $numero;
+$numero **=4;
+echo '<br> Variável recebe atribuição de potencia =' . $numero;
 
 
 ?>
